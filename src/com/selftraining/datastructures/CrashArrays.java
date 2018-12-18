f139fd36bbb7696b7f301091129ad744ae6b22cf
devmohammedothman <mohammed.fci86@gmail.com>
2018-12-18 23:05:20 +0200

    solve two sum problem
    algorithm used hash map to put key,value pairs then check complement exists in keys

--- src/com/selftraining/datastructures/CrashArrays.java
+++ src/com/selftraining/datastructures/CrashArrays.java
@@ -1,5 +1,8 @@
 package com.selftraining.datastructures;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CrashArrays {
 
 	public static void main(String[] args) {
@@ -10,16 +13,30 @@ public class CrashArrays {
 		
 //		System.out.println("Test Result Traverse String : "+result);
 		
-		int arr1[] = {4,6,10};
+//		System.out.println("Test Result Sorted Array ");
+//		
+//		int arr1[] = {4,6,10};
+//		
+//		int arr2[] = {1,2,3,4,9};
+//				
+//		int sorted[] = localObj.mergeSortedArrays(arr1, arr2);
+//		for(int i : sorted)
+//			System.out.println("Item Value "+i);
 		
-		int arr2[] = {1,2,3,4,9};
+		System.out.println("Test Two Sum Algorithm");
+		int [] source = {6,5,7,8,9,3};
+		int result [] = localObj.twoSumOfArrayByMap(source , 10);
 		
-		System.out.println("Test Result Sorted Array ");
+		for(int i : result)
+			System.out.println("Result Value "+i);
 		
-		int sorted[] = localObj.mergeSortedArrays(arr1, arr2);
-		for(int i : sorted)
-			System.out.println("Item Value "+i);
-		
+		//Run with Enhanced HashMap Solution
+//		System.out.println("Test Two Sum Algorithm");
+//		int [] source = {6,5,7,8,9,3};
+//		int result [] = localObj.twoSum(source , 10);
+//		
+//		for(int i : result)
+//			System.out.println("result Value "+i);
 		
 	}
 	 public String traverseArray(String text)
@@ -107,5 +124,112 @@ public class CrashArrays {
 			 return result;
 		 }
 	 }
+	 
+	 /*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+			 You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+			 Example:
+
+			 Given nums = [2, 7, 11, 15], target = 9,
+
+			 Because nums[0] + nums[1] = 2 + 7 = 9,
+			 return [0, 1].
+	 */
+	 //not valid solution because of saving array values as key depend on no duplicates in source array but this may happen
+	 //but enhanced to be working as required by looping over source array itself not keys of map
+	 public int[] twoSumOfArrayByMap(int [] source , int target)
+	 {
+		 int [] result = new int [2];
+		 
+		 //input validation check
+		 if(source == null || source.length == 0 || source.length < 2)
+			 return null;
+		 
+		 int firstItem = source[0];
+		 int secondItem = source[1];
+		 int length = source.length;
+		 int lastItem = source[length - 1 ];
+		 int secondLastItem = source[length - 2 ];
+		 
+		 if(source.length == 2 && ((target - firstItem) == secondItem))
+		 {
+			result[0] = 0 ;
+			result [1]  = 1;
+			return result;
+		 } 
+		 else if(source.length == 2)
+			 return null;
+		 else
+		 {
+			//Best Scenario Check
+			 if((target - firstItem)== secondItem)
+			 {
+			 	result[0] = 0 ;
+				result [1]  = 1;
+				return result;
+			 }
+			 
+			 if((target - lastItem) == secondLastItem)
+			 {
+				 result[0] = length - 1 ;
+				result [1] = length - 2;
+				return result;
+			 }
+			 
+			 //Start Algorithm
+			 
+			 //1st put array source in Map with Key value pair
+			 HashMap<Integer,Integer> sourceMap = new HashMap<>();
+			 for (int i = 0 ; i < length ; i ++)
+			 {
+				 //Fill Map
+				 sourceMap.put(source[i], i);
+				 System.out.println("Filled Map" + sourceMap);
+			 }
+			 
+			 //check if target - map key is exist
+			 for(int i = 0 ;i < length ; i ++)
+			 {
+				 int currentItemValue = i;
+				 System.out.println("Current Item Index " + currentItemValue);
+				 int complement = target - source[i];
+				 System.out.println("complement " + complement );
+				 if( sourceMap.containsKey(complement) && sourceMap.get(complement) != i)
+				 {
+					 result[0] = i;
+					 //return index of complement
+					 result[1] = sourceMap.get(complement);
+					 return result;
+				 }
+			 }
+		 }
+		 
+		 return result;
+	 }
+	 
+	 //enhanced solution with hash map
+	 public int[] twoSum(int[] nums, int target) {
+		    Map<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        map.put(nums[i], i);
+		    }
+		    System.out.println("Filled Map" + map);
+		    
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        
+		        int currentItemValue = i;
+				 System.out.println("Current Item Index " + currentItemValue);
+				 
+				 System.out.println("complement " + complement );
+				 //&& map.get(complement) != i this condition means that the complement is not number it self and this known by check it is not exist in same place i
+		        if (map.containsKey(complement) && map.get(complement) != i ) {
+		            return new int[] { i, map.get(complement) };
+		        }
+		    }
+		    //Cover all possible checks for not valid inputs
+		    throw new IllegalArgumentException("No two sum solution");
+		}
 
 }
